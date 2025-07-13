12103fe7f0e8c5a51346abebd8520ec9bfe33105
hahaha11222 <133786548+hahaha11222@users.noreply.github.com>
2025-07-14 00:22:10 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,13 @@
+// Your Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAh6ouh1bQ0UGhpYEHfHA28xxIL4tcZ3u0",
+  authDomain: "lovesync-aud.firebaseapp.com",
+  databaseURL: "https://lovesync-aud-default-rtdb.firebaseio.com",
+  projectId: "lovesync-aud",
+  storageBucket: "lovesync-aud.firebasestorage.app",
+  messagingSenderId: "835779093008",
+  appId: "1:835779093008:web:259b42c63f31e6d063a095"
+};
 
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
